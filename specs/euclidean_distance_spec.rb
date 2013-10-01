0c59cb91530bf54565bc0bd03acd2de69216dbf8
Lucas Angelino <lucas.angelino@gmail.com>
2013-10-01 20:15:15 -0300

    Melhoria na legibilidade do teste

--- specs/euclidean_distance_spec.rb
+++ specs/euclidean_distance_spec.rb
@@ -4,16 +4,16 @@ describe EuclideanDistance do
 
   it "returns a distance-based similarity score for person1 and person2" do
     ed = EuclideanDistance.new
-
     distance = ed.sim_distance(critics, 'Lisa Rose', 'Gene Seymour')
+
     distance.should be_within(1.0e-12).of(0.148148148148)
   end
 
   it "return 0 if they have no ratings in common" do
     ed = EuclideanDistance.new
+    distance = ed.sim_distance(critics, 'Lisa Rose', 'Jhon')
 
-    ed.sim_distance(critics, 'Lisa Rose', 'Jhon').should == 0
-
+    distance.should == 0
   end
 
 end

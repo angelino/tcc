5fc9cfc4a794033d4da116e20e43f1cce6cfb837
Lucas Angelino <lucas.angelino@gmail.com>
2013-10-01 21:16:24 -0300

    Implementacao do algoritimo Pearson Correlation

--- lib/pearson_correlation.rb
+++ lib/pearson_correlation.rb
@@ -6,24 +6,65 @@ class PearsonCorrelation
       shared_items[k] = 1 if prefs[person2][k]
     end
 
+    # puts shared_items
+
     # if they have no ratings in common, return 0
     return 0 if shared_items.size == 0
 
+
     # Add up all the preferences
     sum1 = 0
-    prefs[person1].each do |k, v|
-      puts "#{self.class} - CHAVE - #{k}"
-      puts "#{self.class} - VALOR - #{v}"
-      sum1 += v
+    shared_items.each do |k, v|
+      sum1 += prefs[person1][k]
     end
-    puts sum1
+    # puts sum1
 
     sum2 = 0
-    prefs[person2].each do |k, v|
-      puts "#{self.class} - CHAVE - #{k}"
-      puts "#{self.class} - VALOR - #{v}"
-      sum2 += v
+    shared_items.each do |k, v|
+      sum2 += prefs[person2][k]
     end
-    puts sum2
+    # puts sum2
+
+    # Sum up the squares
+    squares1 = []
+    shared_items.each do |k, v|
+      squares1 << (prefs[person1][k] ** 2)
+    end
+
+    sum1Sq = 0
+    squares1.each do |s|
+      sum1Sq += s
+    end
+    # puts sum1Sq
+
+    squares2 = []
+    shared_items.each do |k, v|
+      squares2 << (prefs[person2][k] ** 2)
+    end
+
+    sum2Sq = 0
+    squares2.each do |s|
+      sum2Sq += s
+    end
+    # puts sum2Sq
+
+    # Sum up the products
+    products = []
+    shared_items.each do |k , v|
+      products << (prefs[person1][k] * prefs[person2][k])
+    end
+
+    pSum = 0
+    products.each do |p|
+      pSum += p
+    end
+
+    # Calculate Pearson score
+    num = (pSum - ((sum1 * sum2) / shared_items.size))
+    den = Math.sqrt(((sum1Sq - (sum1 ** 2 / shared_items.size)) * (sum2Sq - (sum2 ** 2 / shared_items.size))))
+
+    return 0 if den == 0
+
+    return (num / den)
   end
 end
